ffc26c41a6648b5eb9fba67b44446d4af8b72f3b
MariusMokymai <testams752@gmail.com>
2024-03-29 11:20:47 +0200

    get all + get 1

--- src/routes/jobsRoutes.js
+++ src/routes/jobsRoutes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { dbQueryWithData } = require('../helper');
 
 const jobsRouter = express.Router();
 
@@ -6,7 +7,41 @@ const jobsRouter = express.Router();
 // GET /jobs grazina visas darbo vietas
 jobsRouter.get('/', async (req, res) => {
   // gauti is db visus darbus ir atsiusti juos kaip atsakyma
-  res.json('get all job adds');
+
+  const sql = 'SELECT * FROM jobs';
+
+  const [dataArr, errorJeiYr] = await dbQueryWithData(sql);
+
+  if (errorJeiYr) {
+    console.log('errorJeiYr ===', errorJeiYr);
+    return res.status(500).json('bad bad thing happen');
+  }
+
+  console.log('dataArr ===', dataArr);
+
+  return res.json(dataArr);
+});
+
+// GET /jobs/:id grazina viena darbo vieta
+jobsRouter.get('/:jobId', async (req, res) => {
+  // gauti is db visus darbus ir atsiusti juos kaip atsakyma
+  const { jobId } = req.params;
+  const sql = 'SELECT * FROM jobs WHERE id = ?';
+
+  const [dataArr, errorJeiYr] = await dbQueryWithData(sql, [jobId]);
+
+  if (errorJeiYr) {
+    console.log('errorJeiYr ===', errorJeiYr);
+    return res.status(500).json('bad bad thing happen');
+  }
+
+  console.log('dataArr ===', dataArr);
+
+  if (dataArr.length === 0) {
+    return res.status(404).end();
+  }
+
+  return res.json(dataArr[0]);
 });
 
 module.exports = jobsRouter;
